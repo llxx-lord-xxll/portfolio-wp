65c578f7bf623f077d83315b841f8fa1612fc771
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2021-01-31 00:40:13 +0600

    Bundeling Required Plugins

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
  */
 
 require_once 'modules/is-debug.php';
+require_once 'tgmp/class-tgm-plugin-activation.php';
 
 /*------------------------------------*\
     External Modules/Files
@@ -735,3 +736,99 @@ function portfoliowp_customize_register( $wp_customize ) {
     ) );
 }
 add_action( 'customize_register', 'portfoliowp_customize_register' );
+
+
+add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ *
+ *  <snip />
+ *
+ * This function is hooked into tgmpa_init, which is fired within the
+ * TGM_Plugin_Activation class constructor.
+ */
+function my_theme_register_required_plugins() {
+    /*
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
+    $plugins = array(
+
+        // This is an example of how to include a plugin bundled with a theme.
+        array(
+            'name'      => 'Advanced Custom Fields',
+            'slug'      => 'advanced-custom-fields',
+            'required'  => true,
+        ),
+        array(
+            'name'               => 'ACF Repeater', // The plugin name.
+            'slug'               => 'acf-repeater', // The plugin slug (typically the folder name).
+            'source'             => get_stylesheet_directory() . '/tgmp/plugins/acf-repeater.zip', // The plugin source.
+            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+            'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        ),
+
+        // This is an example of how to include a plugin from the WordPress Plugin Repository.
+        array(
+            'name'      => 'Advanced Bootstrap Blocks',
+            'slug'      => 'advanced-bootstrap-blocks',
+            'required'  => true,
+        ),
+        array(
+            'name'      => 'Genesis Custom Blocks',
+            'slug'      => 'genesis-custom-blocks',
+            'required'  => true,
+        ),
+        array(
+            'name'               => 'Gutenberg Section Block', // The plugin name.
+            'slug'               => 'gutenberg-section-block-master', // The plugin slug (typically the folder name).
+            'source'             => get_stylesheet_directory() . '/tgmp/plugins/gutenberg-section-block-master.zip', // The plugin source.
+            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+            'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        ),
+
+        // <snip />
+    );
+
+    /*
+     * Array of configuration settings. Amend each line as needed.
+     *
+     * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+     * strings available, please help us make TGMPA even better by giving us access to these translations or by
+     * sending in a pull-request with .po file(s) with the translations.
+     *
+     * Only uncomment the strings in the config array if you want to customize the strings.
+     */
+    $config = array(
+        'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+        'default_path' => '',                      // Default absolute path to bundled plugins.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'parent_slug'  => 'themes.php',            // Parent menu slug.
+        'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+        'message'      => '',                      // Message to output right before the plugins table.
+        /*
+        'strings'      => array(
+            'page_title'                      => __( 'Install Required Plugins', 'theme-slug' ),
+            'menu_title'                      => __( 'Install Plugins', 'theme-slug' ),
+            // <snip>...</snip>
+            'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+        )
+        */
+    );
+
+    tgmpa( $plugins, $config );
+
+}
