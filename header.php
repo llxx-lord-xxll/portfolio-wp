e3724f04038477e080398a8f9c9fe8cda96159f0
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2021-01-31 01:25:11 +0600

    Responsiveness fix

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
